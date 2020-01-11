a2ad3df942e961fae7486fe2e4221217daf9e421
You Name <tadeudanilo46@gmail.com>
2020-01-11 13:51:47 -0300

    instalando pacote npm e aplicado o cli-color no node.js

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ let a = "Hello "
 let b = "Mundo"
 let c = ","
 let d = " Danilo!"
+let clc = require("cli-color")
 
 function mensagen(a, b, c, d) {
-    console.log(a + b + c + d)
+    console.log(clc.red.bgWhite.underline(a + b + c + clc.green(d)))
 }
 
 mensagen(a, b, c, d)
